763b382a4bb506135f21c0659731e2faadce0f4c
Paulo Turco <pauloturcko@gmail.com>
2025-10-12 18:42:57 -0300

    feat: #0005 → Implementa lógica de negócio pt.01

---
+++ Domain/Config/FarmConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace DataFarm.Api.Domain.Config;
+
+public class FarmConfig
+{
+    public int Id { get; set; }
+    public int MaxAnimalCapacity { get; set; }
+    public int MaxStockCapacity { get; set; }
+    public int MinLotSizeBuy { get; set; }
+}
