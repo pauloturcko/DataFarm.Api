e1e6d26e20eb4339d40bf96aee77ace4e89b7d94
Paulo Turco <paulo.turco@grupom8.local>
2025-10-15 16:02:15 -0300

    feat: #0007 → Implementa lógica para registrar peso e atualizar informações do animal

--- Application/Services/AnimalService.cs
+++ Application/Services/AnimalService.cs
@@ -2,7 +2,9 @@
 using DataFarm.Api.Domain.Config;
 using DataFarm.Api.Domain.Stock;
 using DataFarm.Api.Application.Repositories;
+using DataFarm.Api.Domain.Animais.Aggregates;
 using DataFarm.Api.Domain.Exceptions;
+using DataFarm.Api.DTOs;
 using DataFarm.Api.Repositories;
 
 namespace DataFarm.Api.Application.Services;
@@ -58,6 +60,66 @@ public class AnimalService : IAnimalService
         }
         
         return animals;
+    }
+
+    public async Task<bool> DeleteAnimalAsync(int id)
+    {
+        var animal = await _animalRepository.GetByIdAsync(id);
+
+        if (animal == null)
+        {
+            throw new NotFoundException("Animal", id);
+        }
+        
+        _animalRepository.Delete(animal);
+
+        return await _animalRepository.SaveChangesAsync();
+    }
+
+    public async Task<Animal?> UpdateAnimalAsync(int id, UpdateAnimalRequest request)
+    {
+        var animal = await _animalRepository.GetByIdAsync(id);
+
+        if (animal == null)
+        {
+            throw new NotFoundException("Animal", id);
+        }
+
+        if (request.DataChegada.HasValue)
+        {
+            animal.DataChegada = request.DataChegada.Value;
+        }
+
+        if (request.Raca.HasValue)
+        {
+            animal.Raca = request.Raca.Value;
+        }
+        
+        _animalRepository.Update(animal);
+
+        return await _animalRepository.SaveChangesAsync() ? animal : null;
+    }
+
+    public async Task<Animal?> RegisterWeightAsync(RegisterWeightRequest request)
+    {
+        var animal = await _animalRepository.GetByIdAsync(request.AnimalId);
+        
+        if(animal == null)
+        {
+            throw new NotFoundException("Animal", request.AnimalId);
+        }
+
+        var novoRegistroPeso = new Peso
+        {
+            PesoAnimal = request.PesoAnimal,
+            DataRegistro = DateOnly.FromDateTime(DateTime.Now),
+        };
+        
+        animal.HistoricoPeso.Add(novoRegistroPeso);
+        
+        _animalRepository.Update(animal);
+        
+        return await _animalRepository.SaveChangesAsync() ? animal : null;
     }
     
 }
---
+++ DTOs/RegisterWeightRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataFarm.Api.DTOs;
+
+public class RegisterWeightRequest
+{
+    // O ID do animal ao qual este peso pertence (Chave para a busca)
+    [Required(ErrorMessage = "O ID do animal é obrigatório.")]
+    public int AnimalId { get; set; }
+    
+    // O peso que está sendo registrado
+    [Required(ErrorMessage = "O peso do animal é obrigatório.")]
+    [Range(0.1, 9999.99, ErrorMessage = "O peso deve ser um valor positivo válido.")]
+    public double PesoAnimal { get; set; }
+}
